f17979dcecfd9b8170bffdd18f21d98d19abfedf
chinaxiaobin <345820392@qq.com>
2019-02-20 21:19:20 +0800

    add python-base

---
+++ python-base/python-基础-01/helloworld.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+print("helloworld")
+"""
+小括号提前成对写，双引号提前成对写，然后输入内容
+如果有中文注释，需要在开头写上#coding=utf-8, 单行注释是#，多行注释'''xxx''' 或 """xxx"""
+
+python 进入交互模式，可以用来调试测试
+python3 进入python3
+ipython  在用python语法的基础上，可以使用系统命令
+ipython3  推荐使用
