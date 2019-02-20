013ddcc34442dff5d21c1f165fc7b74d027cec75
chinaxiaobin <345820392@qq.com>
2019-02-20 22:30:20 +0800

    python-day1

--- python-base/python-基础-01/helloworld.py
+++ python-base/python-基础-01/helloworld.py
@@ -1,10 +1,11 @@
 #coding=utf-8
 print("helloworld")
-"""
+'''
 小括号提前成对写，双引号提前成对写，然后输入内容
-如果有中文注释，需要在开头写上#coding=utf-8, 单行注释是#，多行注释'''xxx''' 或 """xxx"""
+如果有中文注释，需要在开头写上#coding=utf-8, 单行注释是#，多行注释三对单引号或三对双引号
 
 python 进入交互模式，可以用来调试测试
 python3 进入python3
 ipython  在用python语法的基础上，可以使用系统命令
 ipython3  推荐使用
+'''
