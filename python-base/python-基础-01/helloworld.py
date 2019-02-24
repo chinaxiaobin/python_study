54619afdc647ceb55c5bd03ab5571f6f5c595049
chinaxiaobin <345820392@qq.com>
2019-02-24 09:36:25 +0800

    modify helloworld

--- python-base/python-基础-01/helloworld.py
+++ python-base/python-基础-01/helloworld.py
@@ -2,10 +2,16 @@
 print("helloworld")
 '''
 小括号提前成对写，双引号提前成对写，然后输入内容
-如果有中文注释，需要在开头写上#coding=utf-8, 单行注释是#，多行注释三对单引号或三对双引号
+如果有中文注释，需要在开头写上#coding=utf-8,python2需要写，python3是支持的，不用写 
+单行注释是#，多行注释三对单引号或三对双引号
+
 
 python 进入交互模式，可以用来调试测试
 python3 进入python3
 ipython  在用python语法的基础上，可以使用系统命令
 ipython3  推荐使用
+
+安装ipython
+pyenv local 3.5.2/envs/libin
+pip3.5 install ipython  #在这个版本下使用ipython
 '''
